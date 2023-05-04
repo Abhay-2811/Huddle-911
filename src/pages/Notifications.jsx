de9dfd67ab992bd160efa4ec20db6a59302a9aab
Abhay Upadhyay <97840499+Abhay-2811@users.noreply.github.com>
2023-05-04 21:19:24 +0530

    Update Notifications.jsx

--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -33,8 +33,10 @@ const Notifications = () => {
         <h1>Notifications</h1><span>By - Push Protocol</span>
         <h2>
         They are supposed to be here : <br/>
-        - Doctors and clients getting meet link before meet <br/><span>(Can still be seen as they are sent through smart contract, just add <a href='https://chrome.google.com/webstore/detail/push-staging-protocol-alp/bjiennpmhdcandkpigcploafccldlakj'>Push Staging extension</a>)</span><br/> 
+        - Doctors and clients getting meet link before meet <br/><span>(Can still be seen as they are sent through smart contract, just add <a href='https://chrome.google.com/webstore/detail/push-staging-protocol-alp/bjiennpmhdcandkpigcploafccldlakj'>Push Staging extension</a>)</span><br/>
+        Example meet link : https://huddle-911.vercel.app/meet?id=klt-lbyq-opz&date=2023-05-03T10:30:00.000Z
         - Doctors getting link to update documents <br/><span>(This won't show up in push notifications as they were sne through backend)</span>
+        Example update doc link : https://huddle-911.vercel.app/submitReport?id=2023-05-03T10:30:00.000Z
         </h2>
     </div>
   )
